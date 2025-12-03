421d6b7ce27610df1987d2198c151bcfcd229b1c
jinx6ix <irayaian15@gmail.com>
2025-12-03 21:32:56 +0300

    adjusted  sitemap final one

--- app/sitemap.xml/route.tsx
+++ app/sitemap.xml/route.tsx
@@ -62,7 +62,7 @@ ${allPages.map(page => `  <url>
 
     return new Response(xml, {
       headers: {
-        "Content-Type": "application/xml",
+        "Content-Type": "application/xml; charset=utf-8",
         "Cache-Control": "public, max-age=3600, s-maxage=3600",
       },
     })
