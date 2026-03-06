98cfb331fe9dcf92c4d41a0bc1695ca5af695371
jinx6ix <irayaian15@gmail.com>
2026-03-06 14:50:30 +0300

    removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -311,10 +311,6 @@ export default function RootLayout({
         
         {/* Preconnect to critical origins */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
