340dad3a2368f09d330f3f09905b1423507763ed
jinx6ix <irayaian15@gmail.com>
2026-03-28 10:54:36 +0300

    added more images

--- app/safari/1-day-maasai-mara/page.tsx
+++ app/safari/1-day-maasai-mara/page.tsx
@@ -197,6 +197,10 @@ export default function OneDayMaasaiMaraPage() {
         "/gallery-1.jpg",
         "/gallery-2.jpg",
         "/gallery-3.jpg",
+        "/gallery-4.jpg",
+        "/gallery-5.jpg",
+        "/gallery-6.jpg",
+        "/gallery-7.jpg",
       ]
     },
     video: {
