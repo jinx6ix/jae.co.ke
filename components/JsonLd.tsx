b2ad853359b292ef3d79ed8ca12696388f6dc792
jinx6ix <irayaian15@gmail.com>
2026-09-24 22:53:02 +0300

    ...

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,6 +1,10 @@
-export const JsonLd = ({ data }: { data: object }) => (
-    <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify({ "@context": "https://schema.org", ...data }) }}
-    />
-);
+export default function JsonLd({ data }: { data: object }) {
+    return (
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify({ "@context": "https://schema.org", ...data }) }}
+        />
+    )
+}
+
+export { JsonLd }
